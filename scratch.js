59956b1e81e7a3054f115b1b5bde5f3e959f2268
lwzhang <656615764@qq.com>
2015-01-20 18:00:22 +0800

    modify

--- scratch.js
+++ scratch.js
@@ -42,12 +42,12 @@
         },
         event: function () {
             var that = this;
-            var div = doc.querySelector(".div");
+            var upCanvas = this.upCanvas;
             var callback1, callback2, callback3;
-            div.addEventListener("mousedown", callback1 = function (evt) {
-                div.addEventListener("mousemove", callback2 = function (evt) {
-                    var x = evt.clientX;
-                    var y = evt.clientY;
+            upCanvas.addEventListener("mousedown", callback1 = function (evt) {
+                upCanvas.addEventListener("mousemove", callback2 = function (evt) {
+                    var x = evt.clientX - upCanvas.offsetLeft;
+                    var y = evt.clientY - upCanvas.offsetTop;
                     var ctx = that.upCtx;
                     var options = that.options;
                     ctx.beginPath();
@@ -59,34 +59,34 @@
                     ctx.arc(x, y, options.radius, 0, Math.PI * 2, true);
                     ctx.fill();
                     ctx.closePath();
-                    if (that.result() > 0.5) {
+                    if (that.result() > 0.8) {
                         alert(that.award);
-                        div.removeEventListener("mousemove", callback2);
+                        upCanvas.removeEventListener("mousemove", callback2);
                     }
                 }, false);
                 doc.addEventListener("mouseup", callback3 = function () {
-                    div.removeEventListener("mousemove", callback2);
+                    upCanvas.removeEventListener("mousemove", callback2);
                     doc.removeEventListener("mouseup", callback3);
                 }, false);
             }, false);
         },
         result: function () {
-            var textWidth = this.options.text.fontSize * 3;
+            var textWidth = this.options.text.fontSize * this.award.length;
             var textHeight = this.options.text.fontSize;
             // 获取文字部分的像素，这样可以根据刮开文字部分的百分比来提示结果，比如说在刮开50%的时候提示刮奖结果
             var imgData = this.upCtx.getImageData(this.width / 2 - textWidth / 2, this.height / 2 - textHeight / 2, textWidth, textHeight);
-            var pixles = imgData.data;
-            var transPixs = [];
-            for (var i = 0, j = pixles.length; i < j; i += 4) {
+            var pixelsArr = imgData.data;
+            var transPixelsArr = [];
+            for (var i = 0, j = pixelsArr.length; i < j; i += 4) {
                 // a代表透明度
-                var a = pixles[i + 3];
+                var a = pixelsArr[i + 3];
                 // 渐变的透明度＜=0.5，其实透明度的值是介于0~255之间的，0.5 * 255 = 127.5就是a的值
                 if (a < 128) {
-                    transPixs.push(a);
+                    transPixelsArr.push(a);
                 }
             }
             // 小于128的透明度的值的个数占总透明度的的个数的百分比
-            return transPixs.length / (pixles.length / 4);
+            return transPixelsArr.length / (pixelsArr.length / 4);
         }
     };
 
